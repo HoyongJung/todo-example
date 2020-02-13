1a802034239924cbb95bd140a3a853acdef313fe
Hoyong Jung <hjung@Hoyongs-MacBook-Pro.local>
2020-02-13 16:42:27 -0700

    Set up heroku, teamcity, mongo, and github page

--- client/src/config.js
+++ client/src/config.js
@@ -1,11 +1,11 @@
 export default {
-  defaultUserId: 'patrik-drean'
+  defaultUserId: 'hoyong=jung'
 }
 export const configOptions = {
   development: {
     apiUrl: 'http://localhost:3001'
   },
   production: {
-    apiUrl: 'https://todo-patrik-test.herokuapp.com'
+    apiUrl: 'https://hoyong-todo.herokuapp.com'
   }
 }
